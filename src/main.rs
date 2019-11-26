5a438df4c32e50002ffa0185b973d00e827a8b5e
Romanos Skiadas <rski@arista.com>
2019-11-26 23:44:32 +0000

    collect all monitors and pretend to keep highest_res

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,20 @@ struct Monitor {
     name: String,
     connected: bool,
     primary: bool,
+    highest_res: Option<String>,
 }
 
 fn main() {
+    let mut mons: Vec<Monitor> = Vec::new();
+
     let output = process::Command::new("xrandr")
         .output()
         .expect("could not run xrandr");
     let current_setup = std::str::from_utf8(&output.stdout).expect("could not get output");
-    for line in current_setup.lines() {
+    let mut curr_max_res: Option<String> = None;
+    for line in current_setup.lines().rev() {
         if line.starts_with(" ") {
+            curr_max_res = get_res(line);
             continue;
         }
         if line.starts_with("Screen ") {
@@ -24,8 +29,14 @@ fn main() {
             name: v[0].to_owned(),
             connected: v[1] == "connected",
             primary: v[2] == "primary",
+            highest_res: curr_max_res.clone(),
         };
-        println!("{:?}", d);
+        mons.push(d);
     }
-    // println!("setup: {}", current_setup);
+    println!("setup: {:?}", mons);
+}
+
+fn get_res(line: &str) -> Option<String> {
+    println!("{:?}", line);
+    None
 }
