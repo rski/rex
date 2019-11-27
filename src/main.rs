9c32db297917f70cc0b59030417680727e066f5d
Romanos Skiadas <rski@arista.com>
2019-11-27 00:04:19 +0000

    actually get the max resolution out

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 use std::process;
 
 #[derive(Debug)]
-struct Monitor {
+struct Monitor<'a> {
     name: String,
     connected: bool,
     primary: bool,
-    highest_res: Option<String>,
+    highest_res: Option<&'a str>,
 }
 
 fn main() {
     let mut mons: Vec<Monitor> = Vec::new();
-
     let output = process::Command::new("xrandr")
         .output()
         .expect("could not run xrandr");
     let current_setup = std::str::from_utf8(&output.stdout).expect("could not get output");
-    let mut curr_max_res: Option<String> = None;
+    let mut curr_max_res: Option<&str> = None;
     for line in current_setup.lines().rev() {
         if line.starts_with(" ") {
             curr_max_res = get_res(line);
@@ -29,14 +28,14 @@ fn main() {
             name: v[0].to_owned(),
             connected: v[1] == "connected",
             primary: v[2] == "primary",
-            highest_res: curr_max_res.clone(),
+            highest_res: curr_max_res,
         };
         mons.push(d);
     }
     println!("setup: {:?}", mons);
 }
 
-fn get_res(line: &str) -> Option<String> {
-    println!("{:?}", line);
-    None
+fn get_res(line: &str) -> Option<&str> {
+    let mut parts = line.split_ascii_whitespace();
+    parts.nth(0)
 }
