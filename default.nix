bbc30368ef534ead6ea22b5372df4af9c533845d
Romanos Skiadas <rski@arista.com>
2020-07-02 21:44:37 +0100

    fix reproducability issue
    according to
    https://nix.dev/anti-patterns/language.html#attr1-attr2-merge-operator.
    
    nix-env -i -f ./default.nix still works and doesn't name the package
    rex-dev-dev either.

--- default.nix
+++ default.nix
@@ -1,9 +1,8 @@
 with import <nixpkgs>{};
 
 rustPlatform.buildRustPackage rec {
-    pname = "rex-${version}";
-    version = "dev";
-    src = ./.;
+    name= "rex";
+    src = builtins.path { path = ./.; name = "rex"; };
 
     cargoSha256 = "1nk56f5n9zkc2rjnzcaiis2yp00s8zx1dacrvxm74spazqfkxq5r";
 }
