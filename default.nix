fe8febb68632f6b9df0740448ab764e42288541f
Romanos Skiadas <rski@arista.com>
2020-07-10 01:08:59 +0100

    add rustfmt to propagatedBuildInputs
    this way I can run nix-shell and then it'll be there. Magic.

--- default.nix
+++ default.nix
@@ -4,5 +4,7 @@ rustPlatform.buildRustPackage rec {
     name= "rex";
     src = builtins.path { path = ./.; name = "rex"; };
 
+    propagatedBuildInputs = [ rustfmt ];
+
     cargoSha256 = "1nk56f5n9zkc2rjnzcaiis2yp00s8zx1dacrvxm74spazqfkxq5r";
 }
