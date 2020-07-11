57314619a35e88a7885db8a7a30069840ac4ae00
Romanos Skiadas <rski@arista.com>
2020-07-11 13:30:58 +0100

    fix the long path dumping issues
    lifted directly from
    https://christine.website/blog/drone-kubernetes-cd-2020-07-10. The
    issue seems to be related to the target dir.

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
 let
-   pkgs = import <nixpkgs>{};
+  pkgs = import <nixpkgs>{};
+  srcNoTarget = dir:
+    builtins.filterSource
+    (path: type: type != "directory" || builtins.baseNameOf path != "target")
+    dir;
 in
 pkgs.rustPlatform.buildRustPackage rec {
     name= "rex";
-    src = builtins.path { path = ./.; name = "rex"; };
+    src = srcNoTarget ./.;
 
     propagatedBuildInputs = [ pkgs.rustfmt ];
 
