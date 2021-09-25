db3b93342796f70b27a4bdce160fd12497fdfef5
Romanos Skiadas <rom.skiad@gmail.com>
2021-09-25 08:22:05 +0300

    remove rustfmt from default.nix

--- default.nix
+++ default.nix
@@ -2,7 +2,5 @@ with import <nixpkgs> { };
 rustPlatform.buildRustPackage rec {
   name = "rex";
   src = nix-gitignore.gitignoreSource [ ] ./.;
-
-  propagatedBuildInputs = [ rustfmt ];
   cargoLock = { lockFile = ./Cargo.lock; };
 }
